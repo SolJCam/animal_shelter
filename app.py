d0a4b290932ab667f83d684fa558feb8c587bd1b
SolJCam <scameron110@gmail.com>
2021-09-21 18:33:26 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome to Queens animal shelter!!"
